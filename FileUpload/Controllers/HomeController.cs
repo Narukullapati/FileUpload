99b2b371a81c0443944bcbcd451357df6b5cab8b
Narukullapati <harikrishna.narukullapati@hotmail.com>
2016-09-20 20:52:54 -0400

    File Upload

---
+++ FileUpload/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FileUpload.Models;
+
+namespace FileUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/FileUpload
+
+        public ActionResult FileUpload()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+
+        public void Index(IEnumerable<HttpPostedFileBase> files, string command)
+        {
+            if (command == "upload")
+            {
+                if (files != null)
+                {
+                    foreach (var file in files)
+                    {
+                        // Verify that the user selected a file
+                        if (file != null && file.ContentLength > 0)
+                        {
+                            // extract only the fielname
+                            var fileName = Path.GetFileName(file.FileName);
+                            // TODO: need to define destination
+                            var path = Path.Combine(Server.MapPath("~/Upload"), fileName);
+                            file.SaveAs(path);
+                           
+                        }
+                    }
+                }
+                else
+                {
+                    
+                }
+                
+            }
+            else if (command == "save")
+            {
+                if (files != null)
+                {
+                    foreach (var file in files)
+                    {
+                        // Verify that the user selected a file
+                        if (file != null && file.ContentLength > 0)
+                        {
+                            // extract only the fielname
+                            var fileName = Path.GetFileName(file.FileName);
+                            // TODO: need to define destination
+                            var path = Path.Combine(Server.MapPath("~/Uploadr"), fileName);
+                            file.SaveAs(path);
+                        }
+                    }
+                }
+              
+                   
+                
+            }
+           
+        }
+
+  
+    }
+}
---
+++ FileUpload/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FileUpload.Models
+{
+    public class User
+    {
+        public string userid { get; set; }
+        public string username { get; set; }
+    }
+}
